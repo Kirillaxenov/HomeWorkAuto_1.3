334311f0bc085452abd9fce647fe0d11ba73fd8c
Kirill <kaxyonov@gmail.com>
2023-06-16 17:42:07 +0300

    successful test

--- src/test/java/PostmanEchoTest.java
+++ src/test/java/PostmanEchoTest.java
@@ -18,7 +18,7 @@ public class PostmanEchoTest {
 // Проверки
             .then()
             .statusCode(200)
-            .body("data", equalTo("мой мозг уже ага"))
+            .body("data", equalTo("мой мозг уже взорвался"))
     ;
 }
 }
